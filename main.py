a0b9116301a9acd632e3e04cd4b2026a4918bca3
Praneel <140725262+Spectral-Phoenix@users.noreply.github.com>
2024-02-13 15:27:42 +0000

    new file:   __pycache__/nav.cpython-310.pyc 	modified:   main.py 	new file:   nav.py 	modified:   pages/review.py

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 import streamlit as st
+from nav import switch_page
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+api_gemini = "AIzaSyB4v74PmXcRPT33W5aVpROQBSqawQQC6hI"
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-pro",temperature=0.5, google_api_key=api_gemini
+)
 
 st.title("Insight AI")
 
 st.markdown("This is a place where you can save your thoughts, ideas, inspiration, and more.")
 
-txt = st.text_area(
-    "Jot down your thoughts",
-    "It was the best of times, it was the worst of times, it was the age of "
-    "wisdom, it was the age of foolishness, it was the epoch of belief, it "
-    "was the epoch of incredulity, it was the season of Light, it was the "
-    "season of Darkness, it was the spring of hope, it was the winter of "
-    "despair, (...)",
-    )
+text = st.text_area("",key="text")
 
-st.write(f'You wrote {len(txt)} characters.')
+st.write(f'You wrote {len(text)} characters.')
 
-if st.button("Save",use_container_width=True, type="primary"):
-    st.balloons()
-    st.success("Your thoughts have been saved!")
-    st.page_link("pages/review.py", label="Review")
+if st.button("Save", use_container_width=True, type="primary"):
+    # Check if txt length is greater than 20 characters
+    if len(text) > 50:
+        st.divider()
+        col1, col2 = st.columns([6,4])
+        with col1:
+            st.header("🪄 Today in Brief: ")
+            # Create prompt and message with txt
+            prompt1 = '''from the journal entry, identify important events that happened today, 
+                        donot include uncompleted tasks or events'''
+            message = prompt1 + text
+            summary = llm.invoke(message)
+            st.markdown(summary.content)
+        with col2:
+            st.header("✅ Actions: ")
+            prompt2 = '''From the journal entry Provided, identify the incomplete tasks or actions 
+                        that need to be completed and return them, if there are no tasks, then return a message saying no tasks'''
+            message = prompt2 + text
+            summary = llm.invoke(message)
+            st.markdown(summary.content)
+    else:
+        st.info("Please enter more than 20 characters before summarizing.")
